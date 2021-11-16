860c9671a0d3395403f8da8dcbe820facb883dc7
Documenter.jl <documenter@juliadocs.github.io>
2021-11-16 20:24:20 +0000

    build based on 6d3193c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MathepiaModels","category":"page"},{"location":"#MathepiaModels","page":"Home","title":"MathepiaModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MathepiaModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MathepiaModels]","category":"page"}]
+}
